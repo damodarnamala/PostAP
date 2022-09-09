bd9bf34039442d86b82d6b261117e51bc8f00b47
Damodar <damodar.namala@outlook.com>
2022-09-09 16:29:02 +0530

    Initial Commit

---
+++ PostApp/PostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAppApp.swift
+//  PostApp
+//
+//  Created by Damodar Namala on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
